26aba4cc91cfbc8e3fece6706ad7e495a2c3ce8f
rdiego26 <rdieg26@gmail.com>
2020-05-12 19:34:20 -0300

    structure

---
+++ app/src/main/java/me/diegoramos/ceep/ui/ListNotesActivity.kt
@@ -0,0 +1,13 @@
+package me.diegoramos.ceep.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.diegoramos.ceep.R
+
+class ListNotesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.list_notes_activity)
+    }
+}
